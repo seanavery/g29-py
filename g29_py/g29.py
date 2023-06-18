c49adc2f7fe864c9f1bdcddccff929bf6d84b027
seanavery <seanpollock3344@gmail.com>
2023-06-18 18:04:28 -0400

    increase timeout for wheel cal

--- g29_py/g29.py
+++ g29_py/g29.py
@@ -22,7 +22,7 @@ class G29:
         # wheel calibration
         self.device.write(bytes([0xf8, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00]))
         self.device.write(bytes([0xf8, 0x09, 0x05, 0x01, 0x01, 0x00, 0x00]))
-        time.sleep(5) # wait for calibration
+        time.sleep(10) # wait for calibration
 
     def wheel_init(self):
         return 0
