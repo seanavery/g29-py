998d9eb4bf67a10011f130c8b66d71fd36718db8
seanavery <seanpollock3344@gmail.com>
2023-06-23 23:18:01 -0400

    write tests

--- tests/test_g29.py
+++ tests/test_g29.py
@@ -3,8 +3,56 @@ import time
 from g29py import G29
 
 def test_init():
+    g29 = None
+    try:
+        g29 = G29()
+    except:
+        assert False
+    finally:
+        assert g29 is not None
+
+def test_reset():
     g29 = G29()
     assert g29 is not None
+    g29.reset()
+    assert True # did not throw
+
+# TODO: test_connect
+
+# WRITE
+
+def test_force_off():
+    g29 = G29()
+    g29.force_off(0xf3)
+    assert True # did not throw
+
+# def test_force_constant():
+#     g29 = G29()
+#     # g29.connect()
+#     g29.force_constant(0.7)
+
+def test_force_friction():
+    g29 = G29()
+    g29.force_friction(0.5)
+    assert True # did not throw
+    time.sleep(5)
+    g29.force_off(0xf3)
+    assert True # did not throw
+
+def test_set_range():
+    g29 = G29()
+    g29.set_range(500)
+    assert True # did not throw
+    time.sleep(5)
+    g29.set_range(900) # set back to full range
+    assert True # did not throw
+
+def test_set_autocenter():
+    g29 = G29()
+    g29.set_autocenter(0.5, 0.5)
+    assert True # did not throw
+    time.sleep(5)
+    g29.autocenter_off()
 
 # def test_init_g29():
 #     g29 = G29()
