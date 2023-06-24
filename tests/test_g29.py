f48a2e023d1199f1a4c358625e6021abe82d5c0c
seanavery <seanpollock3344@gmail.com>
2023-06-23 23:24:28 -0400

    write tests

--- tests/test_g29.py
+++ tests/test_g29.py
@@ -10,6 +10,9 @@ def test_init():
         assert False
     finally:
         assert g29 is not None
+        assert g29.state is not None
+        assert g29.Cache is None
+
 
 def test_reset():
     g29 = G29()
@@ -53,16 +56,13 @@ def test_set_autocenter():
     assert True # did not throw
     time.sleep(5)
     g29.autocenter_off()
+    assert True # did not throw
 
-# def test_init_g29():
+# READ
+# def test_pump():
 #     g29 = G29()
-#     # g29.force_constant(0.5)
-#     # g29.force_friction(0.5)
-#     # g29.set_range(500)
-#     # g29.set_autocenter(0.1, 0.2)
-#     # g29.autocenter_off()
-#     g29.start_pumping()
-    
-#     while 1:
-#         print(g29.get_state()['steering'])
-#         time.sleep(0.01)
+#     dat = g29.pump()
+#     print(dat)
+#     assert True # did not throw
+#     assert g29.cache is not None
+#     print(g29.cache)
