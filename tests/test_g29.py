65ceed1cbe8aa717bb857601b6ce4ae4aab5afcc
seanavery <seanpollock3344@gmail.com>
2023-06-23 22:38:28 -0400

    starting tests

--- tests/test_g29.py
+++ tests/test_g29.py
@@ -1,17 +1,20 @@
 import time
 
-from g29_py import G29
+from g29py import G29
 
-def test_init_g29():
+def test_init():
     g29 = G29()
-    g29.connect()
-    # g29.force_constant(0.5)
-    # g29.force_friction(0.5)
-    # g29.set_range(500)
-    # g29.set_autocenter(0.1, 0.2)
-    # g29.autocenter_off()
-    g29.start_pumping()
+    assert g29 is not None
+
+# def test_init_g29():
+#     g29 = G29()
+#     # g29.force_constant(0.5)
+#     # g29.force_friction(0.5)
+#     # g29.set_range(500)
+#     # g29.set_autocenter(0.1, 0.2)
+#     # g29.autocenter_off()
+#     g29.start_pumping()
     
-    while 1:
-        print(g29.get_state()['steering'])
-        time.sleep(0.01)
+#     while 1:
+#         print(g29.get_state()['steering'])
+#         time.sleep(0.01)
